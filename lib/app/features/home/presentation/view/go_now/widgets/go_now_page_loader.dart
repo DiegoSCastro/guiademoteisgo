bc0efe1e629bf645e2fdc47f1b3ad467080622f5
Diego Castro <omegadiegocastro@gmail.com>
2025-02-09 14:12:45 -0300

    fix(pubspec): remove unnecessary libs

--- lib/app/features/home/presentation/view/go_now/widgets/go_now_page_loader.dart
+++ lib/app/features/home/presentation/view/go_now/widgets/go_now_page_loader.dart
@@ -6,58 +6,60 @@ class GoNowPageLoader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(24),
-          child: Row(
-            children: [
-              SizedBox.square(
-                dimension: 60,
-                child: AppShimmer(
-                  height: 60,
-                  width: 60,
-                  borderRadius: BorderRadius.circular(30),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(24),
+            child: Row(
+              children: [
+                SizedBox.square(
+                  dimension: 60,
+                  child: AppShimmer(
+                    height: 60,
+                    width: 60,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
                 ),
-              ),
-              const Expanded(
-                child: AppShimmer(
-                  height: 40,
+                const Expanded(
+                  child: AppShimmer(
+                    height: 40,
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ),
-        const AppCard(
-          margin: EdgeInsets.symmetric(horizontal: 24),
-          child: AppShimmer(
-            height: 250,
-            margin: EdgeInsets.only(
-              top: 4,
-              right: 4,
-              left: 4,
-              bottom: 30,
+              ],
             ),
           ),
-        ),
-        const SizedBox(height: 2),
-        const AppShimmer(
-          height: 60,
-          margin: EdgeInsets.symmetric(horizontal: 24, vertical: 2),
-        ),
-        const AppShimmer(
-          height: 60,
-          margin: EdgeInsets.symmetric(horizontal: 24, vertical: 2),
-        ),
-        const AppShimmer(
-          height: 60,
-          margin: EdgeInsets.symmetric(horizontal: 24, vertical: 2),
-        ),
-        const AppShimmer(
-          height: 60,
-          margin: EdgeInsets.symmetric(horizontal: 24, vertical: 2),
-        ),
-      ],
+          const AppCard(
+            margin: EdgeInsets.symmetric(horizontal: 24),
+            child: AppShimmer(
+              height: 250,
+              margin: EdgeInsets.only(
+                top: 4,
+                right: 4,
+                left: 4,
+                bottom: 30,
+              ),
+            ),
+          ),
+          const SizedBox(height: 2),
+          const AppShimmer(
+            height: 60,
+            margin: EdgeInsets.symmetric(horizontal: 24, vertical: 2),
+          ),
+          const AppShimmer(
+            height: 60,
+            margin: EdgeInsets.symmetric(horizontal: 24, vertical: 2),
+          ),
+          const AppShimmer(
+            height: 60,
+            margin: EdgeInsets.symmetric(horizontal: 24, vertical: 2),
+          ),
+          const AppShimmer(
+            height: 60,
+            margin: EdgeInsets.symmetric(horizontal: 24, vertical: 2),
+          ),
+        ],
+      ),
     );
   }
 }
